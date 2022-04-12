4a4031bd308387c2699fee05f5b7eef5f1cdfb28
cousinsjustin <103441434+cousinsjustin@users.noreply.github.com>
2022-04-12 13:49:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,24 @@ import numpy as np
 
 import pandas as pd
 
+def upload_video(file):
+    
+    if file is not None:
+        
+        mace = io.BytesIO(file.read())   
+        
+        temporary_location = ".datasimple5.mp4"
+        
+        with open(temporary_location, 'wb') as out:  
+            
+            out.write(mace.read())  
+    
+        out.close()
+        
+        ss.split_videos_to_frames(temporary_location)
+    
+
+
 
 
 
